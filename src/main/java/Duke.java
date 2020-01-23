f0672c5bf7b54c35bfcc4e95ba22b06c851411fe
NinoMii <e0323600@u.nus.edu>
2020-01-23 21:52:28 +0800

    add add, list for level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,42 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        Scanner scanner = new Scanner(System.in);
-        String byeMsg = "Byebye *waves* See you again! *smiles*";
+    private static String logo = " ____        _        \n"
+                               + "|  _ \\ _   _| | _____ \n"
+                               + "| | | | | | | |/ / _ \\\n"
+                               + "| |_| | |_| |   <  __/\n"
+                               + "|____/ \\__,_|_|\\_\\___|\n";
+
+    private static void initialise(Scanner scanner) {
         String welcomeMsg = "Hello from\n" + logo;
         welcomeMsg += Parser.parse("Hello, I'm Duke! What's your name?");
         System.out.println(welcomeMsg);
         String name = scanner.nextLine().strip();
         welcomeMsg = Parser.parse("Hello " + name + "! What can I do for you?");
         System.out.println(welcomeMsg);
+    }
+
+    private static void echo(String input) {
+        System.out.println(Parser.parse(input));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        initialise(scanner);
         while (scanner.hasNext()) {
             String input = scanner.nextLine();
             if (input.toLowerCase().equals("bye")) {
-                System.out.println(Parser.parse(byeMsg));
+                System.out.println(Parser.parse("Byebye *waves* See you again! ^^"));
                 return;
+            } else {
+                if (input.toLowerCase().equals("list")) {
+                    TaskList.list();
+                } else {
+                    if (TaskList.addTask(input)) {
+                        System.out.println(Parser.parse("added: " + input));
+                    }
+                }
             }
-            System.out.println(Parser.parse(input));
         }
     }
 }

70ff4929ec7fc6c8d04087405c435d945af31415
NinoMii <e0323600@u.nus.edu>
2020-02-16 16:30:39 +0800

    Minor fixes

--- src/main/java/duke/DataParser.java
+++ src/main/java/duke/DataParser.java
@@ -7,7 +7,7 @@ public class DataParser {
     public static final String NEWLINE = "\n";
     //    public static final String SPACE = " ";
 //    public static final String PIPE = "\\|";
-    public static final String SPACED_PIPE = "\\|";
+    public static final String SPACED_PIPE = " \\| ";
     
     /**
     * Parses data (in file format) into a list of tasks.
@@ -24,18 +24,18 @@ public class DataParser {
             }
         
             String[] itemBreakdown = item.split(SPACED_PIPE);
-            String type = itemBreakdown[0].strip();
-            String name = itemBreakdown[2].strip();
-            boolean isCompleted = itemBreakdown[1].strip().equals("1");
+            String type = itemBreakdown[0];
+            String name = itemBreakdown[2];
+            boolean isCompleted = itemBreakdown[1].equals("1");
             switch (type) {
             case "T":
                 tasks.add(new Todo(name, isCompleted));
                 break;
             case "D":
-                tasks.add(new Deadline(name, isCompleted, itemBreakdown[3].strip()));
+                tasks.add(new Deadline(name, isCompleted, itemBreakdown[3]));
                 break;
             case "E":
-                tasks.add(new Event(name, isCompleted, itemBreakdown[3].strip()));
+                tasks.add(new Event(name, isCompleted, itemBreakdown[3]));
                 break;
             default:
                 break;
